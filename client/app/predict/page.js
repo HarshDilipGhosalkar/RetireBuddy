5209758f77ffe0d9d66f7418569e252391e80244
Jhenil Parihar <jhenilparihar@gmail.com>
2023-11-20 21:42:09 +0530

    commit

--- client/app/predict/page.js
+++ client/app/predict/page.js
@@ -8,6 +8,7 @@ const PredictCorpus = () => {
   const [data, setData] = useState({});
   const [step, setStep] = useState(1);
   const [isLoading, setisLoading] = useState(false);
+  const [response, setResponse] = useState({});
 
   const addExpenses = (list, totalExpense) => {
     setData((prevData) => ({
@@ -39,24 +40,35 @@ const PredictCorpus = () => {
     myHeaders.append("Content-Type", "application/json");
     var body = data;
     body["current_health_conditions"] = healthData;
-    var raw = JSON.stringify(data);
+    var raw = JSON.stringify(body); // Fix this line, should be raw = JSON.stringify(body);
     var requestOptions = {
       method: "POST",
       headers: myHeaders,
       body: raw,
       redirect: "follow",
     };
-    setisLoading(true);
-    await fetch("http://localhost:5000/predictRetirementCorpus", requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.log("error", error))
-      .finally(() => {
-        setisLoading(false);
-      });
-  };
 
-  const sendPostRequest = () => {};
+    setisLoading(true);
+    try {
+      const response = await fetch(
+        "http://localhost:5000/predictRetirementCorpus",
+        requestOptions
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok.");
+      }
+
+      const responseData = await response.json();
+      console.log(responseData); // Check the response data
+
+      // Assuming setResponse is a state updater function
+      setResponse(responseData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setisLoading(false);
+    }
+  };
 
   console.log(step);
   console.log(data);
@@ -67,7 +79,9 @@ const PredictCorpus = () => {
       ) : step == 2 ? (
         <AddHealthRecord handleSubmit={addHealthRecord} />
       ) : (
-        <>{!isLoading ? <FinalReport list={result} /> : <h1>Loading...</h1>}</>
+        <>
+          {!isLoading ? <FinalReport list={response} /> : <h1>Loading...</h1>}
+        </>
       )}
     </>
   );
